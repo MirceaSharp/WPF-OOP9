fddb89ef2aa6f419998ecbe6628c192cb7a0b26b
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-02-14 23:47:32 +0100

    Second commit

---
+++ WPF OOP9/ControlPanel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_OOP9
+{
+    class ControlPanel
+    {
+      
+        public virtual void TurnLightsOn()
+        {
+
+        }
+        public virtual void TurnLightsOff()
+        {
+
+
+        }
+        public virtual void TurnHeatingOn()
+        {
+
+
+        }
+
+        public virtual void TurnHeatingOFf()
+        {
+
+
+
+        }
+
+
+    }
+}
