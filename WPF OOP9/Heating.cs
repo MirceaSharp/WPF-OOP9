f9c33df321f4da89f5c5a1730cf5cc493dff6386
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-02-14 23:52:11 +0100

    Update

---
+++ WPF OOP9/Heating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_OOP9
+{
+    class Heating
+    {
+    }
+}
